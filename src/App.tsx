e7a6300717332942a05f8ac3b212ccf3a72feb7a
Jhonata <gutemberg.jhonata@gmail.com>
2021-05-14 08:08:20 -0300

    Create Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <>
-      <h1>Hello!</h1>
-      <GlobalStyle></GlobalStyle>
+      <Header />
+      <GlobalStyle />
     </>
   );
 }
