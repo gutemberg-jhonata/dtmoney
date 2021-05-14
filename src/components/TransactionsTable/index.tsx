85aed24e7898a5b78c93e73954a7bec701bfd777
Jhonata <gutemberg.jhonata@gmail.com>
2021-05-14 18:39:59 -0300

    Add Axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,13 +1,12 @@
 import { useEffect } from "react";
+import { api } from "../../services/api";
 
 import { Container } from "./styles";
 
 export function TransactionsTable() {
   useEffect(() => {
-    fetch('https://localhost:3000/api/transactions')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
+    api.get("transactions").then((response) => console.log(response.data));
+  }, []);
 
   return (
     <Container>
@@ -49,5 +48,5 @@ export function TransactionsTable() {
         </tbody>
       </table>
     </Container>
-  )
+  );
 }
