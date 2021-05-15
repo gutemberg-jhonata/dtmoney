f259e2f8df410d46344356f62873032de58642ad
Jhonata <gutemberg.jhonata@gmail.com>
2021-05-15 20:21:35 -0300

    Add Style to RadioBox

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from 'polished';
+import { darken, transparentize } from "polished";
 
 export const Container = styled.form`
   h2 {
@@ -12,8 +12,8 @@ export const Container = styled.form`
     padding: 0 1.5rem;
     height: 4rem;
 
-    background: #E7E9EE;
-    border: 1px solid #D7D7D7;
+    background: #e7e9ee;
+    border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
 
     font-weight: 400;
@@ -38,7 +38,7 @@ export const Container = styled.form`
 
     border: 0;
     border-radius: 0.25rem;
-    
+
     margin-top: 1.5rem;
     font-size: 1rem;
     font-weight: 600;
@@ -56,34 +56,45 @@ export const TransactionTypeContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
+`;
 
-  button {
-    height: 4rem;
-    border: 1px solid #D7D7D7;
-    border-radius: 0.25rem;
-    
-    background: transparent;
+type RadioBoxProps = {
+  isActive: boolean;
+  activeColor: "green" | "red";
+};
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+const colors = {
+  green: "#33CC95",
+  red: "#E52E4D",
+};
 
-    transition: border-color 0.2s;
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
 
-    &:hover {
-      border-color: ${darken(0.1, "#D7D7D7")};
-    }
+  background: ${({ isActive, activeColor }) =>
+    isActive ? transparentize(0.9, colors[activeColor]) : ""};
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  transition: border-color 0.2s;
+
+  &:hover {
+    border-color: ${darken(0.1, "#D7D7D7")};
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `;
