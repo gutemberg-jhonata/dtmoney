ea03635b3b8abefbb460c1cb65f967c7a626a001
Jhonata <gutemberg.jhonata@gmail.com>
2021-05-15 18:52:09 -0300

    Add Style to NewTransactionModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import Modal from "react-modal";
 import { GlobalStyle } from "./styles/global";
-
-Modal.setAppElement('#root');
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
@@ -24,22 +22,10 @@ export function App() {
 
       <Dashboard />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-        <form>
-          <input type="text" />
-          <input type="number" />
-          <div>
-            <button type="button">Entrada</button>
-            <button type="button">Saída</button>
-          </div>
-          <input type="text" />
-          <button type="submit">Cadastrar</button>
-        </form>
-      </Modal>
+      />
 
       <GlobalStyle />
     </>
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -52,4 +52,41 @@ export const GlobalStyle = createGlobalStyle`
     opacity: 0.6;
     cursor: not-allowed;
   }
+
+  .react-modal-overlay {
+    background: rgb(0, 0, 0, 0.5);
+    
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+    background: var(--background);
+    width: 100%;
+    max-width: 576px;
+    padding: 3rem;
+    border-radius: 0.25rem;
+    
+    position: relative;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    border: 0;
+    background: transparent;
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
+  }
 `;
