af9af76046d81edcf41d91970abf3da897e2649c
Jhonata <gutemberg.jhonata@gmail.com>
2021-05-16 13:13:34 -0300

    Format data createdAt and amount

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,45 @@ new Server({
     transaction: Model
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Desenvolvimento de site",
+          amount: 12000,
+          type: "deposit",
+          category: "Venda",
+          createdAt: new Date("2021-04-13")
+        },
+        {
+          id: 2,
+          title: "Hamburguer",
+          amount: 59,
+          type: "withdraw",
+          category: "Alimentação",
+          createdAt: new Date("2021-04-10")
+        },
+        {
+          id: 3,
+          title: "Aluguel do apartamento",
+          amount: 1200,
+          type: "withdraw",
+          category: "Casa",
+          createdAt: new Date("2021-03-27")
+        },
+        {
+          id: 4,
+          title: "Computador",
+          amount: 5400,
+          type: "deposit",
+          category: "Venda",
+          createdAt: new Date("2021-03-15")
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = "api";
 
